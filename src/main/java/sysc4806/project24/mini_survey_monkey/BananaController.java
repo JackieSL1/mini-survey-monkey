52cf068a13eff8ed40d8d25ef5a1bd1dccc1a1c9
Braeden Kloke <braedenkloke@gmail.com>
2024-11-03 17:50:04 -0500

    Add health check endpoint
    Banana.

---
+++ src/main/java/sysc4806/project24/mini_survey_monkey/BananaController.java
@@ -0,0 +1,17 @@
+package sysc4806.project24.mini_survey_monkey;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+/**
+ * Health check with bananas.
+ */
+public class BananaController {
+
+    @GetMapping("/banana")
+    public String banana(Model model) {
+        return "banana";
+    }
+}
