40497ab98fc9ef0f27a35b61c25adc34e38feaf8
Braeden Kloke <braedenkloke@gmail.com>
2024-11-03 17:30:39 -0500

    Init Spring app.

---
+++ src/test/java/sysc4806/project24/mini_survey_monkey/MiniSurveyMonkeyApplicationTests.java
@@ -0,0 +1,13 @@
+package sysc4806.project24.mini_survey_monkey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniSurveyMonkeyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
